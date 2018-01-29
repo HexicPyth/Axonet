18ef7f19a5e6907ea4d4cf2b0b15388973e2993d
Hexic Pyth <hexicpyth@gmail.com>
2018-01-29 21:38:24 +0000

    ...

--- src/client/client.py
+++ src/client/client.py
@@ -151,6 +151,7 @@ class Client:
 
         if sig not in message_list:
             message_list.append(sig)
+            print('broadcasting: '+full_message)
             self.broadcast(full_message)
 
     def listen(self, in_socket):
