404c6580c5d35a01dc02f2bf70771cd4eed18d50
Hexic Pyth <hexicpyth@gmail.com>
2019-10-25 19:45:38 +0000

    Revert "Start implementing Algorithmatic Mesh Network Bootstrapping Algorithm"
    This reverts commit b9ee698580e9c41daa80a23735181f982278960b.

--- src/client/init_client.py
+++ src/client/init_client.py
@@ -9,7 +9,7 @@ def init(network_architecture):
     x = client.Client()
     x.initialize(port=port, net_architecture=network_architecture, remote_addresses=None,
                  command_execution=True, default_log_level="Debug", modules=["corecount"],
-                 networkSize=6)
+                 networkSize=5)
 
 
 if __name__ == "__main__":
