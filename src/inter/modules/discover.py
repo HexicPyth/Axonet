52bbf67b96b332899d364d0ba918361428a6cbf2
Hexic Pyth <hexicpyth@gmail.com>
2019-10-25 18:12:14 +0000

    Patch bug in peer discovery module

--- src/inter/modules/discover.py
+++ src/inter/modules/discover.py
@@ -33,7 +33,7 @@ def respond_start(net_tuple, op_id, cluster_rep):
     if cluster_rep:
         injector = inject.NetworkInjector()
         injector.broadcast("newpage:"+op_id, net_tuple)  # Create a pagefile to store peer addresses in
-        injector.broadcast("sharepeers:"+op_id)          # Instruct nodes to append all peer addresses to this pagefile
+        injector.broadcast("sharepeers:"+op_id, net_tuple)          # Instruct nodes to append all peer addresses to this pagefile
 
 
 
