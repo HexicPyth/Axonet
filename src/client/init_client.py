a8b24a571fff11e6608f7405b3a9c57ad6a49151
Hexic Pyth <hexicpyth@gmail.com>
2019-11-08 13:28:41 +0000

    Fix typo in default init_client configuration which resulted in error

--- src/client/init_client.py
+++ src/client/init_client.py
@@ -8,8 +8,8 @@ port = 3705
 def init(network_architecture):
     x = client.Client()
     x.initialize(port=port, net_architecture=network_architecture, remote_addresses=None,
-                 command_execution=True, default_log_level="Debug", modules=["corecount"],
-                 networkSize=5)
+                 command_execution=True, default_log_level="Info", modules=["corecount"],
+                 net_size=9)
 
 
 if __name__ == "__main__":
