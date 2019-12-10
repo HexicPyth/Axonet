a6bfd747256d5ee0987fea5bc32aafbdeab45e1e
MikeS3 <MikeSmith9107@gmail.com>
2019-12-10 08:58:56 -0500

    finish implimenting client_confgiruration json

--- src/client/init_client.py
+++ src/client/init_client.py
@@ -8,16 +8,19 @@ port = 3705
 
 with open("client_configuration.json") as client_configuration:
     client_config_data = json.load(client_configuration)
-    for var in client_config_data:
-        print(var, json.loads(var))
-
+    port = client_config_data["port"]
+    remote_addresses = client_config_data["remote_addresses"]
+    command_execution = client_config_data["command_execution"]
+    default_log_level = client_config_data["default_log_level"]
+    modules = client_config_data["modules"]
+    net_size = client_config_data["net_size"]
 
 
 def init(network_architecture):
     x = client.Client()
-    x.initialize(port=port, net_architecture=network_architecture, remote_addresses=None,
-                 command_execution=True, default_log_level="Info", modules=["corecount"], net_size=4,
-                 assigned_part_numbers=[])
+    x.initialize(port=port, net_architecture=network_architecture, remote_addresses=remote_addresses,
+                 command_execution=command_execution, default_log_level=default_log_level, modules=modules,
+                 net_size=net_size)
 
 
 if __name__ == "__main__":
