8e3177b967560b479e2d4b7c20df83a28b1c4341
Michael <mikesmith9107@gmail.com>
2019-12-11 09:52:10 -0500

    add network architecture to client_congiguration.json and init_client

--- src/client/init_client.py
+++ src/client/init_client.py
@@ -4,12 +4,12 @@
 import client
 import json
 
-
 with open("client_configuration.json") as client_configuration:
-
     """Sets each variable equal to the value given in the client_configuration.json file"""
+
     client_config_data = json.load(client_configuration)
     port = client_config_data["port"]
+    network_architecture = client_config_data["network_architecture"]
     remote_addresses = client_config_data["remote_addresses"]
     command_execution = client_config_data["command_execution"]
     default_log_level = client_config_data["default_log_level"]
@@ -17,7 +17,7 @@ with open("client_configuration.json") as client_configuration:
     net_size = client_config_data["net_size"]
 
 
-def init(network_architecture):
+def init():
     x = client.Client()
     x.initialize(port=port, net_architecture=network_architecture, remote_addresses=remote_addresses,
                  command_execution=command_execution, default_log_level=default_log_level, modules=modules,
@@ -25,4 +25,4 @@ def init(network_architecture):
 
 
 if __name__ == "__main__":
-    init("mesh")
+    init()
