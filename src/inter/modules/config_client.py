fa96b1ef58d7ed78f43660290a24dd856df40b59
Michael <mikesmith9107@gmail.com>
2019-12-12 12:17:27 -0500

    fix issues with config port

--- src/inter/modules/config_client.py
+++ src/inter/modules/config_client.py
@@ -11,6 +11,21 @@ sys.path.insert(0, (os.path.abspath('../../inter/misc')))
 import primitives
 
 
+def change_port(arguments):
+
+    try:
+        if int(arguments[2]):
+
+            with open("client_configuration.json")as client_configuration:
+                client_config = json.load(client_configuration)
+                client_config['port'] = arguments[2]
+                json.dumps(client_config, indent=3)
+                print(client_config['port'])
+
+    except (ValueError, TypeError):
+        print("That is not a valid port")
+
+
 def config_argument(arguments, sub_node, log_level):
     _primitives = primitives.Primitives(sub_node, log_level)
     print(arguments, "there are the arguments")
@@ -36,10 +51,10 @@ def config_argument(arguments, sub_node, log_level):
             _primitives.log("Successfully set network_architecture to: " + network_architecture,
                             in_log_level="Info")
     elif arguments[0] == "permanent":
-
+        print("we made it this far")
         if arguments[1] == "port":
-            pass
-            # port(arguments)
+
+            change_port(arguments)
         elif arguments[1] == "network_architecture":
             pass
         elif arguments[1] == "remote_addresses":
@@ -54,22 +69,3 @@ def config_argument(arguments, sub_node, log_level):
             pass
         else:
             print("Error \"" + arguments[1] + "\" isn't correct syntax")
-
-
-"""
-def port(arguments):
-    print("?????????????????")
-    try:
-        if int(arguments[2]):
-
-            with open("client_configuration.json")as client_configuration:
-                client_config = json.load(client_configuration)
-                client_config['port'] = arguments[2]
-                json.dumps(client_config, indent=3)
-                print(client_config['port'])
-        else:
-            print("That is not a valid port")
-    except TypeError:
-        print("That is not a valid port")
-
-"""
